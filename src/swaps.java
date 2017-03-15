bdb1a50eef0eefe76ebb339df96c01d6f2637c7d
papanpapan <samim.skmolla@gmail.com>
2017-03-15 18:21:09 +0530

    Swaps
    Swaps

---
+++ src/swaps.java
@@ -0,0 +1,17 @@
+
+public class swaps {
+
+	
+	public static void main(String[] args) {
+		int a=5; int b=7;
+		System.out.println("Before swap:"+"\n"+"Valu of A:" +a +"\n"+"Valu of B:" +b+"\n");
+		
+		a=a+b;
+		b=a-b;
+	    a=a-b;
+		System.out.println("Afer swap:"+"\n"+"Valu of A:" +a +"\n"+"Valu of B:" +b+"\n");
+		
+
+	}
+
+}
